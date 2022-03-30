6813d01ed120267bc3a59da7f3a77d11012a6997
seyedrezabazyar <seyedrezabazyar@gmail.com>
2022-03-30 18:07:09 +0430

    Completing the MVC project - Delete extra files and code

--- webprogmvc/app/controllers/Pages.php
+++ webprogmvc/app/controllers/Pages.php
@@ -4,15 +4,12 @@ class Pages extends Controller
 {
     public function __construct()
     {
-        $this->ArticleModel = $this->model('Article');
     }
 
     public function index()
     {
-        $articles = $this->ArticleModel->getArticle();
         $data = [
             'title' => 'WebProgMVC',
-            'articles' => $articles
         ];
         $this->view('pages/index', $data);
     }
--- webprogmvc/app/views/pages/index.php
+++ webprogmvc/app/views/pages/index.php
@@ -1,8 +1,3 @@
 <?php require APPROOT . '/views/inc/header.php' ?>
 <h1><?= $data['title'] ?></h1>
-<ul>
-    <?php foreach ($data['articles'] as $article) : ?>
-        <li><?= $article->title ?></li>
-    <?php endforeach ?>
-</ul>
 <?php require APPROOT . '/views/inc/footer.php' ?>
