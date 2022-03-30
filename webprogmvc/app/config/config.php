a88c769c965941542f5765bc98a2c36fe3ae37dd
seyedrezabazyar <seyedrezabazyar@gmail.com>
2022-03-30 17:25:05 +0430

    Database

--- webprogmvc/app/config/config.php
+++ webprogmvc/app/config/config.php
@@ -1,5 +1,11 @@
 <?php
 
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'webprog_mvc');
+
 // App Root
 define('APPROOT', dirname(__DIR__));
 // URL Root
---
+++ webprogmvc/app/libraries/Database.php
@@ -0,0 +1,72 @@
+<?php
+
+/* PDO Database Class
+ *  Connect To Database
+ * Create Prepared Statements
+ * Bind Values
+ * Return rows and values
+ */
+
+class Database
+{
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $dbname = DB_NAME;
+
+    private $dbh;
+    private $stmt;
+    private $error;
+
+    public function __construct()
+    {
+        // SET DNS 
+        $dns = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname . 'charset=utf8';
+        // Create PDO instance
+        try {
+            $this->dbh = new PDO($dns, $this->user, $this->pass);
+            $this->dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        } catch (PDOException $e) {
+            $this->error = $e->getMessage();
+            echo $this->error;
+        }
+    }
+
+    // Prepare Statement with query
+    public function query($sql)
+    {
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+
+    // Bind Value
+    public function bind($param, $value)
+    {
+        $this->stmt->bindParam($param, $value);
+    }
+
+    // Execute the prepared statement
+    public function execute()
+    {
+        return $this->stmt->execute();
+    }
+
+    // Get result set as array of object
+    public function fetchAll()
+    {
+        $this->execute();
+        return $this->stmt->fetchAll();
+    }
+
+    // Get single record as object
+    public function fetch()
+    {
+        $this->execute();
+        return $this->stmt->fetch();
+    }
+
+    // Get row count
+    public function rowCount()
+    {
+        return $this->stmt->rowCount();
+    }
+}
