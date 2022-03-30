a7138d550c43abcc1cf663a64522459d58d29019
seyedrezabazyar <seyedrezabazyar@gmail.com>
2022-03-30 16:17:30 +0430

    Automatically configure files and load

--- webprogmvc/app/bootstrap.php
+++ webprogmvc/app/bootstrap.php
@@ -1,5 +1,16 @@
 <?php
 
-include_once 'libraries/Core.php';
-include_once 'libraries/Controller.php';
-include_once 'libraries/Database.php';
+// Load Config
+require_once 'config/config.php';
+
+// include_once 'libraries/Core.php';
+// include_once 'libraries/Controller.php';
+// include_once 'libraries/Database.php';
+
+
+function webprog_mvc($classname)
+{
+    include_once 'libraries/' . $classname . '.php';
+}
+
+spl_autoload_register('webprog_mvc');
---
+++ webprogmvc/app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+// App Root
+define('APPROOT', dirname(__DIR__));
+// URL Root
+define('URLROOT', 'http://localhost/github/php-mvc/webprogmvc/');
+// Site Name
+define('SITENAME', 'WebProgMVC');
