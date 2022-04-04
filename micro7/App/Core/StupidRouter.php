aa67b77d3885f0ad5049ce90acf77a7e7197c3b9
seyedrezabazyar <seyedrezabazyar@gmail.com>
2022-04-04 13:53:01 +0430

    micro7: mf 2 router implementation

--- micro7/App/Core/StupidRouter.php
+++ micro7/App/Core/StupidRouter.php
@@ -2,11 +2,38 @@
 
 namespace App\Core;
 
+use App\Utilities\Url;
+
 class StupidRouter
 {
     private $routes;
     public function __construct()
     {
-        $this->routes = [];
+        # $route = /colors/red && /colors/green && /colors/blue
+        $this->routes = [
+            '/github/php-mvc/micro7/colors/red' => 'colors/red.php',
+            '/github/php-mvc/micro7/colors/green' => 'colors/green.php',
+            '/github/php-mvc/micro7/colors/blue' => 'colors/blue.php'
+        ];
+    }
+
+    public function run()
+    {
+        $current_route = Url::current_route();
+
+        foreach ($this->routes as $route => $view) {
+            if ($current_route == $route) {
+                $this->includeAndDie(BASEPATH . "views/$view");
+            }
+        }
+        # 404 Header
+        header("HTTP/1.0  404 Not Found");
+        $this->includeAndDie(BASEPATH . "views/errors/404.php");
+    }
+
+    private function includeAndDie($viewPath)
+    {
+        include $viewPath;
+        die();
     }
 }
--- micro7/index.php
+++ micro7/index.php
@@ -1,19 +1,9 @@
 <?php
 # front controller
 
-use App\Utilities\Url;
+use App\Core\StupidRouter;
 
 include 'bootstrap/init.php';
 
-$route = Url::current_route();
-
-# $route = /colors/red && /colors/green && /colors/blue
-
-if ($route == '/github/php-mvc/micro7/colors/red')
-    include BASEPATH . 'views/colors/red.php';
-
-if ($route == '/github/php-mvc/micro7/colors/green')
-    include BASEPATH . 'views/colors/green.php';
-
-if ($route == '/github/php-mvc/micro7/colors/blue')
-    include BASEPATH . 'views/colors/blue.php';
+$router = new StupidRouter();
+$router->run();
